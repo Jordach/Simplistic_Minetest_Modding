3910e772a57be544924cc7e5db27e3fd6168707b
Jordan Snelling <jordach.snelling@gmail.com>
2012-08-25 15:07:20 +0100

    Slightly update again.

--- init.lua
+++ init.lua
@@ -3,8 +3,15 @@
 -- This is a prototype; feel free to add things, or change things to fit.
 --Licence; LGPL
 
-dofile 
+dofile(minetest.get_modpath("path_of_mod").."/.lua")
+
+path_of_mod = 
+-- Add the folder name you would like the path to be, also name the file like mymod.lua
+
+master_modname = 
+-- Place your mod name here, IT MUST BE THE SAME AS THE FOLDER.
 
 
-function basic_node(modname, groups, tiles. description,)
-	minetest.register_node("modname
+
+function basic_node(groups, tiles. description, nodename,)
+	minetest.register_node("modname"
