b28489f2fa359507b5cc54b1e21e6fcfcd9a0067
Jordan Snelling <jordach.snelling@gmail.com>
2012-08-25 15:18:15 +0100

    Add the two other files
    which contain the other mechanics of modding, the tools and crafting.

---
+++ crafting.lua
@@ -0,0 +1,7 @@
+-- Crafting of Simplistic Modding
+-- Part of Simplistic Minetest Modding
+-- By Jordan Snelling
+
+-- This is a empty file for the meanwhile.
+
+-- Don't expect anything just yet.
--- init.lua
+++ init.lua
@@ -1,17 +1,26 @@
---Simple Modding!
+-- Simple Modding!
 -- By Jordach, AKA, Jordan Snelling, http://twitter.com/jordansnelling / jordach.snelling@gmail.com / jordach@blokkeren.co.cc
 -- This is a prototype; feel free to add things, or change things to fit.
---Licence; LGPL
+-- Licence; LGPL
 
-dofile(minetest.get_modpath("path_of_mod").."/.lua")
-
-path_of_mod = 
--- Add the folder name you would like the path to be, also name the file like mymod.lua
-
-master_modname = 
--- Place your mod name here, IT MUST BE THE SAME AS THE FOLDER.
+dofile(minetest.get_modpath("PATH_OF_MOD").."/nodes.lua") -- This file is where the 'blocks' of the game can be found.
+dofile(minetest.get_modpath("PATH_OF_MOD").."/crafting.lua") -- This file contains all of the definitions of the crafting, like so many wood makes a tool, or stick.
+dofile(minetest.get_modpath("PATH_OF_MOD").."/tools.lua") -- This is where your pickaxes. shovels, axes and swords can be found.
 
 
+PATH_OF_MOD = 
+-- Add the folder name you would like the path to be.
 
-function basic_node(groups, tiles. description, nodename,)
-	minetest.register_node("modname"
+MASTER_MODNAME = 
+-- Place your mod name above this line, IT MUST BE THE SAME AS THE FOLDER.
+
+
+
+function basic_node(groups, texture. description, nodename, drawtype)
+	minetest.register_node("modname" .. nodename, {
+		description = description,
+		drawtype = drawtype,
+		tiles = texture,
+		is_ground_content = true,
+		groups = groups,
+		
