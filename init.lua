5f6addbf93273ad006b94500b9627ec3864a8f9e
Jordan Snelling <jordach.snelling@gmail.com>
2012-08-25 15:41:12 +0100

    finish the 'base part'
    this code is untested with any kind of node, and what not.

--- init.lua
+++ init.lua
@@ -3,9 +3,10 @@
 -- This is a prototype; feel free to add things, or change things to fit.
 -- Licence; LGPL
 
-dofile(minetest.get_modpath("PATH_OF_MOD").."/nodes.lua") -- This file is where the 'blocks' of the game can be found.
 dofile(minetest.get_modpath("PATH_OF_MOD").."/crafting.lua") -- This file contains all of the definitions of the crafting, like so many wood makes a tool, or stick.
 dofile(minetest.get_modpath("PATH_OF_MOD").."/tools.lua") -- This is where your pickaxes. shovels, axes and swords can be found.
+-- Removing these two lines and files is a BAD IDEA. The other parts to the thing won't work; e.g. crafting or your tools will not work.
+
 
 
 PATH_OF_MOD = 
@@ -14,13 +15,21 @@ PATH_OF_MOD =
 MASTER_MODNAME = 
 -- Place your mod name above this line, IT MUST BE THE SAME AS THE FOLDER.
 
+function basic_node(groups, texture. description, nodename, sound)
+	minetest.register_node("modname" .. nodename, {
+		description = description,
+		tiles = texture,
+		is_ground_content = true,
+		groups = groups,
+		sounds = default.node_sound_ .. sound .. _defaults(),
+})
 
-
-function basic_node(groups, texture. description, nodename, drawtype)
+function un_natural_node(groups, texture. description, nodename, drawtype, sound)
 	minetest.register_node("modname" .. nodename, {
 		description = description,
 		drawtype = drawtype,
 		tiles = texture,
 		is_ground_content = true,
 		groups = groups,
-		
+		sounds = default.node_sound_ .. sound .. _defaults(),
+})
