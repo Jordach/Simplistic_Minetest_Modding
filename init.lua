61cc12b2ebcb5cb42631302f13b81f9ad6ef2590
Jordan Snelling <jordach.snelling@gmail.com>
2012-08-25 14:56:08 +0100

    Starting comit
    Adding the start framework. this will happen over time.

---
+++ init.lua
@@ -0,0 +1,10 @@
+--Simple Modding!
+-- By Jordach, AKA, Jordan Snelling, http://twitter.com/jordansnelling / jordach.snelling@gmail.com / jordach@blokkeren.co.cc
+-- This is a prototype; feel free to add things, or change things to fit.
+--Licence; LGPL
+
+dofile 
+
+
+function basic_node(modname, groups, tiles. description,)
+	minetest.register_node("modname
